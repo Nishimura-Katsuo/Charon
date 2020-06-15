1649446a51e685a8a75e65be1f150e522e2f283a
Nishimura-Katsuo <jwebbb01@hotmail.com>
2020-06-14 18:27:35 -0600

    Updated gameDraw hook and fixed branding text

--- main.cpp
+++ main.cpp
@@ -11,8 +11,26 @@ using std::wcout;
 using std::cout;
 using std::endl;
 
-void drawStats() {
-    D2::DrawText(L"Charon v0.1", 700, 595, 4, 0);
+void drawStats(bool inGame) {
+    WCHAR msgtext[] = L"Charon v0.1";
+    DWORD width = 0, fileno = 1, screenmode = D2::GetScreenSize(), ScreenWidth = 0, ScreenHeight = 0;
+
+    switch (screenmode) {
+    case 0:
+        ScreenWidth = 640;
+        ScreenHeight = 480;
+        break;
+    case 1:
+    case 2:
+        ScreenWidth = 800;
+        ScreenHeight = 600;
+        break;
+    }
+
+    DWORD old = D2::SetFontFileNumber(1);
+    D2::GetTextSize(msgtext, &width, &fileno);
+    D2::DrawText(msgtext, ScreenWidth - width - 5, ScreenHeight - 5, inGame ? 0 : 4, 0);
+    D2::SetFontFileNumber(old);
 }
 
 // Keeps the game at a steady framerate without using too much CPU.
@@ -34,8 +52,8 @@ void throttle() {
 
 // Since we patch to override this function, we need to call it ourselves.
 void gameDraw() {
-    drawStats();
-    D2::unknown_53B30();
+    drawStats(true);
+    //D2::unknown_53B30();
 }
 
 void gameLoop() {
@@ -44,7 +62,7 @@ void gameLoop() {
 
 // Since we patch to override DrawSprites, we need to call it ourselves.
 void oogDraw() {
-    drawStats();
+    drawStats(false);
     D2::DrawSprites();
 }
 
@@ -71,7 +89,7 @@ void init(std::vector<LPWSTR> argv, DllMainArgs dllargs) {
     PatchCall<5>(Offset::Base + 0xFA663 + 5, throttle);
     SetBytes<13>(Offset::Base + 0xFA663 + 10, 0x90);
 
-    PatchCall<5>(Offset::Base + 0x572C9, gameDraw); // Hook the game draw
+    PatchCall<5>(Offset::Base + 0x4CB14, gameDraw); // Hook the game draw
     PatchCall<5>(Offset::Base + 0xF9A0D, oogDraw); // Hook the oog draw
 
     PatchCall<6>(Offset::Base + 0xF5623, multi); // Allow multiple windows open
@@ -80,6 +98,8 @@ void init(std::vector<LPWSTR> argv, DllMainArgs dllargs) {
     SetBytes<1>(Offset::Base + 0x3BF60, 0xC3); // Prevent battle.net connections
 
     SetBytes<1>(Offset::Base + 0x4846DC, 1); // enables debug printouts
+    
+    *D2::NoPickUp = true;
 
     cout << "Charon loaded." << endl;
 }
