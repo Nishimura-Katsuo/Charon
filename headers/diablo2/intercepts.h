5d1863e5a97cb28d47dafff4711e03936f480ffa
Nishimura-Katsuo <38635886+Nishimura-Katsuo@users.noreply.github.com>
2020-06-15 09:25:59 -0600

    Updated draw hooks

--- headers/diablo2/intercepts.h
+++ headers/diablo2/intercepts.h
@@ -5,8 +5,13 @@
 
 #include "D2Structs.h"
 #include "pointers.h"
+#include <chrono>
+#include <thread>
 
 bool __fastcall gameInput(wchar_t* wMsg);
 void _gameInput();
 HMODULE __stdcall multi(LPSTR Class, LPSTR Window);
 void FTJReduce();
+void _oogDraw();
+void _gameDraw();
+void _throttle();
