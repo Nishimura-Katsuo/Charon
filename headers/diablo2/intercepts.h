9bccf2e2daf76951f62228a1ae01d1095c53db95
Nishimura-Katsuo <38635886+Nishimura-Katsuo@users.noreply.github.com>
2020-06-17 02:11:14 -0600

    Added more draw hooks

--- headers/diablo2/intercepts.h
+++ headers/diablo2/intercepts.h
@@ -10,14 +10,14 @@
 
 const DWORD DEFAULT_FONT = 1;
 
-bool __fastcall gameInput(wchar_t* wMsg);
-void _gameInput();
+BOOL __fastcall chatInput(wchar_t* wMsg);
+void _chatInput();
 HMODULE __stdcall multi(LPSTR Class, LPSTR Window);
-void FTJReduce();
 void _oogDraw();
 void _gameDraw();
 void _throttle();
 void _gameAutomapDraw();
+void _preDrawUnitsPatch();
 int __stdcall printf_newline(const char* format, ...);
 namespace D2 {
     extern int ScreenWidth, ScreenHeight;
