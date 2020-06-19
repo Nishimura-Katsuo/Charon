537f4116147a5aa2bf1906faa40ef3f32a35072e
Nishimura-Katsuo <38635886+Nishimura-Katsuo@users.noreply.github.com>
2020-06-19 09:34:13 -0600

    Rewrote a bunch of stuff

---
+++ headers/diablo2/newpointers.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <windows.h>
+#include <vector>
+#include <functional>
+
+namespace Offset {
+    extern DWORD Base, D2CLIENT, D2COMMON, D2GAME, D2LANG, D2NET, D2MULTI, D2LAUNCH, D2WIN, D2GFX, D2CMP, BNCLIENT, STORM;
+    void DefineOffsets();
+}
+
+#ifdef DEFINE_XREF
+#define XREF ExternalReference
+#else
+#define XREF extern ExternalReference
+#endif
+
+template <DWORD& base, class F, DWORD offset>
+class ExternalReference {
+public:
+    ExternalReference() {}
+
+#pragma warning(suppress: 4180)
+    F* getAddress() {
+        return (F*)(base + offset);
+    }
+
+    operator F& () {
+        return *(F*)(base + offset);
+    }
+
+#pragma warning(suppress: 4180)
+    F& operator= (const F& value) {
+        F* tmp = (F*)(base + offset);
+        *tmp = value;
+        return *tmp;
+    }
+};
+
+/**
+ * D2 Offsets here.
+ */
+
+namespace D2 {
+    XREF<Offset::Base, DWORD __stdcall (), 0xF5160> GetScreenMode;                          // Updated 1.14d //004F5160-BASE
+    XREF<Offset::D2CLIENT, long __fastcall (VOID), 0x5AFC0> GetMouseXOffset;               // Updated 1.14d //0045AFC0-BASE
+    XREF<Offset::D2CLIENT, long __fastcall (VOID), 0x5AFB0> GetMouseYOffset;               // Updated 1.14d //0045AFB0-BASE
+}
