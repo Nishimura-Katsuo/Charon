7aece4dd40b9642740203a87e21d61a30105dbbc
Nishimura-Katsuo <38635886+Nishimura-Katsuo@users.noreply.github.com>
2020-07-02 22:44:16 -0600

    Updated version

--- headers/common.h
+++ headers/common.h
@@ -21,7 +21,7 @@ public:
 std::wstring COLOR(BYTE color);
 
 const DWORD DEFAULT_FONT = 1;
-const std::wstring version = L"Charon v0.97";
+const std::wstring version = L"Charon v1.00";
 extern GameOutput gamelog;
 
 namespace D2 {
